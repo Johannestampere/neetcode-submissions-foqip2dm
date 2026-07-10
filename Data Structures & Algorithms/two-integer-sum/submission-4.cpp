62d1e5dedfac867900b736acab8863e3018fb2fc
Johannes Tampere <80104176+Johannestampere@users.noreply.github.com>
2026-07-10 00:07:14 -0400

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> h;
+        int len = nums.size();
+
+        for (int i = 0; i < len; ++i) {
+            if (!h.contains(target - nums[i])) {
+                h[nums[i]] = i;
+            } else {
+                vector<int> res = {min(i, h[target-nums[i]]), max(i, h[target-nums[i]])};
+                return res;
+            }
+        }
+    }
+};
